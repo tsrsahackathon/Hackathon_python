c0b1d5cb7a85a72df2d5da44cc5916275f47dba6
Riddhiman Yadav <72695232+Darkclown007@users.noreply.github.com>
2023-11-02 15:05:32 +0530

    L

--- summarizationandgpt.py
+++ summarizationandgpt.py
@@ -33,27 +33,36 @@ print(data)
 ##summarization part end
 
 ##GPT part start
+api_key = "0G8JXZAH09BZ1BEVYB09BIJLYB8W2Y4TPDZDI4HI9LNUM9EE9UJ95P0VEZ5RBHDY"
+endpoint = "https://jamsapi.hackclub.dev/openai/chat/completions"
+
 headers = {
-  "api-key": api_key,
-  "content-type": "application/json"
-}
-payload = {
-  "input": "write a speech on the toyota supra",
-  "input_type": "conversation",
-	"content_type": "application/json",
-	"output_type": "json",
-  "multilingual": {
-    "enabled": True
-  },
-  "steps": [
-    {
-      "skill": "gpt"
-    }
-  ],
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
 }
 
-r = requests.post(url, json=payload, headers=headers)
-data = r.json()
-print(data)
+messages = []
+
+chat_over = False
+
+while not chat_over:
+    user_question = input("Enter a question or type 'quit' to end the chat: ")
+    if user_question.lower() == "quit":
+        chat_over = True
+        break
+    messages.append({"role": "user", "content": user_question})
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": messages
+    }
+    response = requests.post(endpoint, headers=headers, json=data)
+    if response.status_code == 200:
+        result = response.json()
+        assistant_answer = result["choices"][0]["message"]["content"]
+        print(assistant_answer)
+        messages.append({"role": "assistant", "content": assistant_answer})
+    else:
+        print(f"Request failed: {response.reason}")
+        chat_over=True
 
 ##GPT part end
